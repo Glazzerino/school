64d4f79a3ad98687c18b250db2f46c3e26682cd9
Glazzerino <=>
2020-04-09 23:04:08 -0500

    check

--- ComputationalBiology/Act2/main.R
+++ ComputationalBiology/Act2/main.R
@@ -1,6 +1,6 @@
 # install.packages("stringr")
-library(stringr)
-library(MASS)
+
+# library(MASS)
 nuevos_pacientes <- read.csv("E:/Code/school/ComputationalBiology/Act2/NUEVO_INGRESO.csv")
 # Revisa si los datos cargados son del tipo DataFrame
 ans <- "no"
@@ -10,20 +10,18 @@ if (is.data.frame(nuevos_pacientes)) {
 print(paste("El archivo ", ans, " es DataFrame\n"));
 cat(paste("Observaciones: ", nrow(nuevos_pacientes)),"\n")
 cat(paste("Variables: ", nrow(nuevos_pacientes) * ncol(nuevos_pacientes)),"\n")
-enfermedades <- unique(nuevos_pacientes$DESCRIPCION.DIAGNOSTICO)
-
+enfermedades <- levels(nuevos_pacientes$DESCRIPCION.DIAGNOSTICO)
 cat(paste("Numero de padecimientos oncologicos: ", length(enfermedades)),"\n")
 
 # Obtenemos los estados con mayor cantidad de incidencias 
-# top10estados <- tail(names(sort(table(nuevos_pacientes$ESTADO))), 10)
-# top10ciudades <- tail(names(sort(table(nuevos_pacientes$MUNICIPIO))), 10)
-
-print(estados_unicos)
-print(paste(estados_unicos, " estados diferentes en el dataframe"))
+print(levels(nuevos_pacientes$ESTADO))
 cat(paste("Estados con mayores incidencias: \n"))
 print(summary(nuevos_pacientes$ESTADO,10))
 cat(paste("Ciudades con mayores incidencias: \n"))
 print(summary(nuevos_pacientes$MUNICIPIO,10))
-cat(paste("Cantidad de estados: ", length(unique(nuevos_pacientes$ESTADO))),"\n")
-# print(summary(nuevos_pacientes))
+cat(paste("Cantidad de estados: ", length(unique(nuevos_pacientes$ESTADO)),"\n"))
+
+# Despliegue de los primeros 6 pacientes
+
+print(head(nuevos_pacientes,6))
 
