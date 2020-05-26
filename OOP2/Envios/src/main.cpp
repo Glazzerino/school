1a8e5a2615ebdec21556b8944ff9170e4718391a
glazz <fztmon@gmail.com>
2020-05-25 20:11:29 -0500

    Envios completo

--- OOP2/Envios/src/main.cpp
+++ OOP2/Envios/src/main.cpp
@@ -1,25 +1,39 @@
 #include "Paquete.h"
+#include "Sobre.h"
 #include <fstream>
-
+#include <iostream>
+using namespace std;
 int main() {
-    Envio *envios[20];
-    ifstream file("../src/data.txt");
+    Envio *envios[100];
+    ifstream file("datosEnvios.txt");
     char type;
     int counter = 0;
-    double peso, largo, ancho, 
-    costo_kg,costo, cargo_adicional;
+    double total_envios = 0;
+    double peso, largo, ancho,
+           costo_kg,costo, cargo_adicional;
     string nombre_r, estado_d, ciudad_d,ciudad_r,estado_r, cp_r, cp_d, nombre_d;
     while(file >> type >> nombre_r >> estado_r >> ciudad_r >> cp_r >> nombre_d >> estado_d >> ciudad_d >> cp_d >> costo) {
         Persona rem(nombre_r,estado_r,ciudad_r,cp_r);
         Persona des(nombre_d,estado_d,ciudad_d,cp_d);
-
+        // cout << type << endl;
         if (type == 'p') {
             file >> peso >> costo_kg;
+            // std::cout << peso << " " << costo_kg << endl;
             envios[counter++] = new Paquete(rem,des,costo,peso, costo_kg);
         }
-        else if (type == 's') {
+        else {
             file >> largo >> ancho >> cargo_adicional;
+            //  cout << largo << ancho << cargo_adicional << endl;
+            envios[counter++] = new Sobre(rem,des,costo,largo, ancho, cargo_adicional);
         }
     }
-    cout << "lol"<< endl;
+    for (int i=0; i<counter; i++) {
+        cout << "Remitente:" << endl;
+        envios[i]->getRemitente().Imprimir();
+        cout << "Destinatario:" << endl;
+        envios[i]->getDestinatario().Imprimir();
+        cout << "Costo: " << envios[i]->calculaCosto() << endl;
+        total_envios += envios[i]->calculaCosto();
+    }
+    cout << "Total envios: " << total_envios << endl;
 }
