95bdadf59400ad83e8bdbed17be6914845fa16f0
glazz <fztmon@gmail.com>
2020-08-18 19:10:58 -0500

    working on mergesort

--- DATASTRUCTS/ordenamiento/src/main.cpp
+++ DATASTRUCTS/ordenamiento/src/main.cpp
@@ -13,7 +13,7 @@ public:
     Timer() {
         start = std::chrono::high_resolution_clock::now();
     };
-    
+
     ~Timer() {
         end = std::chrono::high_resolution_clock::now();
         auto timestep = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
@@ -22,12 +22,12 @@ public:
 private:
     std::chrono::high_resolution_clock::time_point start,end;
 };
-
+int swaps = 0;
 template<class T>
-inline void swap(vector<T>& lista, int a, int b) {
-    int aux = lista[b];
-    lista[b] = lista[a];
-    lista[a] = aux;
+inline void swap(T* a,T* b) {
+    T aux = b;
+    b = a;
+    a = aux;
 }
 
 // O(n2)
@@ -37,33 +37,49 @@ void intercambio(vector<T>& lista, int& iter, int& comps) {
     for (int i=0; i<lista.size(); i++) {
         iter++;
         comps++;
-        cout << "Current num: " << lista[i] << endl;
+        // cout << "Current num: " << lista[i] << endl;
         for (int x=i+1; x<lista.size(); x++) {
             iter++;
             comps++;
             if (lista[x] > lista[i]) {
-                swap(lista,x,i);
+                comps++;
+                swap(lista[x],lista[i]);
             }
         }
     }
 }
 
+//(O*N^2)
 template<class T>
-void bubble(vector<T>& lista) {
-    for (int i=0; i< lista.size()-1; i++) {
-        for (int x=0; x<lista.size()-i-1; x++) {
-            
+void bubble(vector<T>& lista, int& iter, int& comps) {
+    bool swapped = true;
+    Timer timer;
+    for (int i=0; i < lista.size()-1; i++) {
+        iter++;
+        for (int x=0; x < lista.size()-i-1; x++) {
+            swapped=false;
+            iter++;
+            if (lista[x+1]< lista[x]) {
+                swapped = true;
+                comps++;
+                swap(lista[x+1], lista[x]);
+            }
+        }
+        if (!swapped) {
+            break;
         }
     }
 }
 
+
 int main() {
     int iteraciones = 0;
     int comparaciones = 0;
-    vector<int> lista = {4,6489, 1000, 100, 9,8};
-    intercambio<int>(lista, iteraciones, comparaciones);
+    vector<int> lista = {8,7,6,5,4,3,2,};
+    bubble<int>(lista, iteraciones, comparaciones);
     for (int x : lista) {
         cout << x << " ";
     }
-    cout << "Iteraciones: " << iteraciones << " Comparaciones: " << comparaciones << endl;
+    // mergeSort(lista);
+    cout <<endl << "Iteraciones: " << iteraciones << " Comparaciones: " << comparaciones << endl;
 }
---
+++ DATASTRUCTS/ordenamiento/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    int c = 10;
+    c <<=1;
+    std::cout << c << std::endl;
+}
