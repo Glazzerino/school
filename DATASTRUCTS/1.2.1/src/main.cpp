08dae3c678a396a75b7af23678142adf22e70609
Glazzerino <=>
2020-08-28 18:13:45 -0500

    sequential works

--- DATASTRUCTS/1.2.1/src/main.cpp
+++ DATASTRUCTS/1.2.1/src/main.cpp
@@ -2,52 +2,35 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <map>
 using namespace std;
 
-int getUnique(string line) {
-    for (int i=0;i<line.size(); i+=2) {
-        if (line[i] != )
+pair<char, int> sequential(string line) {
+    int comps = 0;
+    int i = 0;
+    for (int i=0; i<line.size(); i+=2) {
+        comps++;
+        if (line[i] != line[i+1]) {
+            return pair<char, int>{line[i], comps};
+        }
+        
     }
+    return pair<char, int>{' ', -1};
 }
 
-template<class T>
-size_t binarySearch(vector<T> list) {
-    // Timer timer;
-    int r, l, m;
-    l = 0;
-    r = list.size()-1;
-    while (r >= l) {
-        int m = l + (r - l) / 2;
-        if (list[m] == query ) {
-            return m;
-        }
-        else if (list[m] < query ) {
-            l = m+1;
-        }
-        else {
-            r = m-1;
-        }
-    }
-    return -1;
-}
-template<class T>
-size_t sequential(vector<T>& list, T query) {
-    size_t x;
-    for (x = 0; x < list.size(); x++) {
-        if (list[x] == query) {
-            return x;
-        }
-    }
-    return -1;
-}
+
 int main() {
     cout << "test" << endl;
     ifstream file;
     file.open("01.in");
-    int lines;
-    file >> lines;
-    stringstream line;
-    for (int i=0; i < lines; i++) {
+    string line;
 
+    while (getline(file, line)) {
+        if  (line.size() == 1) {
+            continue;
+        }
+        auto ans = sequential(line);
+        cout << ans.first  << " " << ans.second << endl;
     }
+    
 }
